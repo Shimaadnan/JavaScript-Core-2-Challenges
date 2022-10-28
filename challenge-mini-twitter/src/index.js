72cb55c822615bf74f09b83384145ce12397764e
Shimaadnan <78430611+Shimaadnan@users.noreply.github.com>
2022-10-28 16:03:33 +0100

    final changes

---
+++ challenge-mini-twitter/src/index.js
@@ -0,0 +1,8 @@
+const form= document.getElementById('form-area');
+form.addEventListener('submit', (event) => {
+ event.preventDefault();
+ const textArea=document.querySelector("#text")
+ const paragragh=document.getElementById("paragragh")
+ let valueEl=textArea.value;
+ paragragh.textContent=valueEl
+});
